f870092f1e77bbc09c54d897bfa1fad0849cdae8
flanger11 <flanger11@gmail.com>
2018-09-20 20:43:25 -0600

    Added defaults

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 class Car {
 
     constructor(brand, model, year) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
+        this.brand = brand || "Jeep";
+        this.model = model || "CJ5";
+        this.year = year || 1999;
     }
 
     DisplayCarInfo() {
@@ -15,6 +15,8 @@ class Car {
 
 const car1 = new Car("Ford", "Escort", 2012);
 const car2 = new Car("GM", "Camero", 2015);
+const car3 = new Car();
 
 
 car1.DisplayCarInfo();
+car3.DisplayCarInfo();
